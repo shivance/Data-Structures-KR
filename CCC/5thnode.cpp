1d8bd0a3c446d520447638eee1fd7b6950579257
Anshuman Mishra <shivanshuman021@gmail.com>
2020-10-08 10:57:25 +0530

    updated

--- CCC/5thnode.cpp
+++ CCC/5thnode.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <climits>
+#include <map>
+#include <cstdlib>
+#include <cstddef>
 using namespace std;
 
 class node
@@ -9,12 +12,10 @@ public:
 	node* next;
 	bool eol;
 
-	/*node(int x)
-	{
-		dt = x;
+	node()
+	{		
 		next = NULL;
-		eol = false;
-	}*/
+	}
 };
 
 void print_list(node* n)
@@ -25,66 +26,99 @@ void print_list(node* n)
 	}
 }
 
+node* inputLL()
+{
+	node* head = new node();
+	node* prev;
+	int a; cin>>a;
+	if (a==-1) 
+	{
+		return NULL;
+	}
+	head->dt = a;node* tmp;
+	prev = head;
+	while(true)
+	{
+		cin>>a;
+		if (a==-1)
+		{
+			cout<<"BROKE\n";
+			break;
+		}
+		else
+		{
+			tmp = new node();
+			cin>>a;
+			tmp->dt = a;
+			prev->next = tmp;
+			prev = tmp;
+		} 
+	}
+
+	return head;
+
+}
 
 
 int main()
 {
-	node* head , end;
-	head = NULL;
-	int a;
-	cin>>a;
-	head = new node();
-	head->dt = a;
-	
-	int cnt = 1;
-	node* fifth = NULL;
-	node* prev = head;
+	int n;
+	cin>>n;
+	node*arr[n] ;
+	for (int i=0;i<n;++i)
+	{
+		arr[i] = inputLL();
+		print_list(arr[i]);
+	}
+
+	//traversing through linkedlists and hashing so as to get the count
+
+	map<int,int> m;
+
 	node* tmp;
-	while(true)
+	for (int i=0;i<n;++i)
 	{
-		cin>>a;
-		
-		if (a==-1)
+		tmp = arr[i];
+		while(tmp->next != NULL)
 		{
-			tmp->eol = true;
-			if (cnt <5)
+			m[tmp->dt]++;
+			tmp = tmp->next;
+		}
+	}
+
+	map<int,int>::iterator it,itr;
+
+	int count[4];count[0] = INT_MAX;
+	int previdx = 0;
+	for (int i=1;i<=3;++i)
+	{
+		int max = INT_MIN;
+
+		for (it = m.begin();it!=m.end();++it)
+		{
+			if (it->second>max && it->second<count[previdx])
 			{
-				cout<<0<<"\n";
+				max = it->second;
 			}
-			else{
-				tmp->next = fifth;
-			}
-			break;
 		}
 
-		else
-		{
-			cnt++;
-			tmp = new node();
-			tmp->dt = a;
-			prev->next = tmp;
-			prev = tmp;
-		}
-
-		if (cnt==5)
-		{
-			fifth = tmp;
-		}
-
+		count[i] = max;
+		previdx = i;
 	}
 
-	//print_list(head);
-	//cout<<"\n\n";
-	
+	// now our count[1,2,3] contain top 3 values of counts
 
-	if (tmp->eol)
-	{	
-		cout<<1<<"\n"<<fifth->dt<<"\n";
-	}
-	else
+	for (int i=1;i<=3;++i)
 	{
-		cout<<0<<"\n";
+		for (it = m.begin();it!=m.end();++it)
+		{
+			if(it->second == count[i])
+			{
+				cout<<it->first<<" "<<it->second<<"\n";
+			}
+		}
 	}
+
 
 	return 0;
 }
