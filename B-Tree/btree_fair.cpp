6c2e7f0d15e7c7863e39788843732594aa1fc2e2
Anshuman Mishra <shivanshuman021@gmail.com>
2020-11-16 17:27:02 +0530

    Btree stage - 2 complete

--- B-Tree/btree_fair.cpp
+++ B-Tree/btree_fair.cpp
@@ -27,10 +27,11 @@ public:
 
 deque<BTnode> nodeCache;
 
-BTnode newBTnode(int d)
+BTnode* newBTnode(int d)
 {
     nodeCache.push_back(BTnode(d));
-    return nodeCache.back();
+    BTnode* node = &nodeCache.back();
+    return node;
 }
 
 void swap(int &a,int&b)
@@ -70,13 +71,10 @@ void inorder(BTnode*BT)
     int i;
     for (i=0;i<BT->cnt;++i)
     {
-        //if(BT->way[i]!=NULL)
         inorder(BT->way[i]);
         if(BT->key[i]!=INT_MAX) 
             cout<<BT->key[i]<<" ";
-        //else break;
     }
-    //if(BT->way[i]!=NULL)
     inorder(BT->way[i]);
 }
 
@@ -93,15 +91,11 @@ void sortL(BTnode*&leaf)
 
 BTnode* search(BTnode*BT,int k)
 {
-    //exist = false;
     int i;
     for (i=0;i<BT->cnt;++i)
     {
         if(BT->key[i]==k)
-        {
-            //exist = true;
             return BT;
-        }
         else if (BT->key[i]>k)
             break;
     }
@@ -111,8 +105,6 @@ BTnode* search(BTnode*BT,int k)
         return BT;
     else 
         return search(BT->way[i],k);
-
-    //return NULL;
 }
 
 
@@ -130,31 +122,19 @@ BTnode* parent(BTnode* BT,BTnode* node)
 
 void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
 {
-    cout<<"Inserting to  : ";printarr(leaf->key,leaf->cnt);cout<<"\n";
+    
     //no overflow
     if (leaf->cnt < d-1)
     {
         leaf->key[leaf->cnt] = k;
         ++(leaf->cnt);
-        //sort(leaf->key,leaf->key+leaf->cnt);
-        sortL(leaf);        
-
-        cout<<"leaf now is : ";
-        printarr(leaf->key,leaf->cnt);
-        
-        int idx = -1;
-
+        sortL(leaf);                
+        int idx;    
         getidx(leaf->key,leaf->cnt,idx,k);
-        cout<<"idx = "<<idx<<"\n";
         ri8 = leaf->way[idx+1];
-        //printarr(ri8->key,ri8->cnt);
-        if (ri8) cout<<"Not null\n";
-        else cout<<"null\n";
-        cout<<"added no overflow k = "<<k<<" to ";printarr(leaf->key,leaf->cnt);cout<<"\n";
-        return;
     }
 
-    else //overflow
+    else 
     {
         int cnt_l = leaf->cnt;
         BTnode* newchild = new BTnode(d);
@@ -167,7 +147,7 @@ void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
         
         leaf->cnt = 0;
         tmp.push_back(k);
-        cout<<"added overflow k = "<<k<<"\n";
+        
         sort(tmp.begin(),tmp.end());
         int mid_idx = tmp.size()/2;
         int mid = tmp[mid_idx];
@@ -176,27 +156,23 @@ void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
         {
             if (tmp[i]<mid) 
             {
-                leaf->key[leaf->cnt] = (tmp.at(i));
+                leaf->key[leaf->cnt] = tmp[i];
                 leaf->cnt++;
             }
             else if (tmp[i]>mid) 
             {
-                newchild->key[newchild->cnt] = (tmp.at(i));
+                newchild->key[newchild->cnt] = tmp[i];
                 newchild->cnt++;
             }
         }
         sortL(newchild);
         sortL(leaf);
-        cout<<"Node splitted into : ";
-        cout<<"Left : ";printarr(leaf->key,leaf->cnt);cout<<" right : ";printarr(newchild->key,newchild->cnt);cout<<"\n";
-        // newly splitted right node
         
         //adjusting pointers
         int j;
         if (mid == k) 
         {
             j= 1;
-            cout<<"mid == k \n";
             o = true; // represents mid == key in parent
         }
         else j= 0;
@@ -204,10 +180,6 @@ void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
         for (int i=mid_idx+1;i<=cnt_l;++i,++j)
         {
             newchild->way[j] = leaf->way[i];
-            if (leaf->way[i]!=NULL){
-                cout<<"newchild->way["<<j<<"] ";
-                printarr(leaf->way[i]->key,leaf->way[i]->cnt);cout<<"\n";
-            }
             leaf->way[i]=NULL;
         }
 
@@ -215,51 +187,34 @@ void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
         //middle to parent
         if (BT==leaf)
         {
-            BTnode* newpar = new BTnode(d);
+            BTnode* newpar = newBTnode(d); 
             newpar->key[0] = mid;
             newpar->cnt++;
             newpar->way[0] = leaf;
             newpar->way[1] = newchild;
-            
-            BT = newpar; // update root
-            
-            
-            cout<<"parent created ";printarr(newpar->key,newpar->cnt);cout<<"\n";
-            cout<<"root updated -- added "<<mid<<" to root\n";
-            cout<<"data of root : ";printarr(BT->key,BT->cnt);
+            BT = newpar; // update root when overflow in root
             ri8 = newchild;
-            cout<<"data of newly splitted node : ";printarr(ri8->key,ri8->cnt);cout<<"\n";
-            cout<<"INORDER OF RI8 is : ";printarr(ri8->key,ri8->cnt);
         }
         else
         {
-            BTnode* par = parent(BT,leaf);
+            BTnode* par = parent(BT,leaf);int idxn;
             
-            cout<<"called nested else \n";
             int c = par->cnt;
-            //ri8 = newchild;
-            //cout<<"data of newly splitted node : ";printarr(ri8->key,ri8->cnt);cout<<"\n";
-            //cout<<"INORDER OF RI8 is : ";printarr(ri8->key,ri8->cnt);
+
             o = false;
             Add(BT,par,ri8,d,mid,o);
-            cout<<"o is "<<o<<"\n";
-            int idxn;
+            
             if (c<d-1){
                 getidx(par->key,par->cnt,idxn,mid);
-                cout<<"added mid = "<<mid<<"\n";
                 par->way[idxn+1] = newchild;
             }
             else
             {
-                cout<<"parent overflow \n";
                 if (o) 
-                {
-                    cout<<"parent INORDER is "; inorder(ri8);cout<<"\n";
                     ri8->way[0] = newchild;
-                }
+
                 else{
                     getidx(ri8->key,ri8->cnt,idxn,mid);
-                    cout<<"added mid = "<<mid<<"\n";
                     ri8->way[idxn+1] = newchild;
                 }
             }
@@ -271,28 +226,30 @@ void Add(BTnode* & BT,BTnode* &leaf,BTnode*&ri8,int d,int k,bool &o)
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     BTnode* BT = NULL,*ri8 = NULL,*leaf = NULL;
-    int k;int d;cin>>d;bool o = false;
+    int k,d,n;bool o;
+    cin>>d;
     cin>>n;
     cin>>k;
-    BT = new BTnode(d);
+    BT = newBTnode(d);
+    // using a deque cache for performance improvement
+    // didn't use 'new BTnode(d)'
     BT->key[BT->cnt] = k;
     BT->cnt++;
 
     for (int i=1;i<n;++i)
     {
         cin>>k;
-        if (k==-1) break;
-
         leaf = search(BT,k);
         ri8 = NULL;
         o = false;
-        
-        cout<<"INSERTING : "<<k<<"\n";
         Add(BT,leaf,ri8,d,k,o);
-        cout<<"Current inorder = ";inorder(BT);
-        cout<<"\n";
     }
+    inorder(BT);
     
     return 0;
 }
