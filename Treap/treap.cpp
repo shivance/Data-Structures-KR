1d82e8af5ff94198216d0791cf64bd5221e4e248
Anshuman Mishra <shivanshuman021@gmail.com>
2020-11-18 15:09:55 +0530

    updated

--- Treap/treap.cpp
+++ Treap/treap.cpp
@@ -1,4 +1,3 @@
-//Minheap Treap BSTSTYLE
 
 #include <iostream>
 #include <climits>
@@ -101,11 +100,13 @@ void del(trpnode* &T)
 		if (T->lc->key < T->rc->key)
 		{
 			T->key = T->lc->key;
+			T->p = T->lc->p;
 			del(T->lc);
 		}
 		else
 		{
 			T->key = T->rc->key;
+			T->p = T->rc->p;
 			del(T->rc);
 		}
 	}
@@ -172,14 +173,30 @@ int main()
 		}
 	}
 	inorder(TRP);
-
-	cin>>k;
-	findDel(TRP,k);
-	inorder(TRP);
-	cout<<"\n"<<TRP->key<<"\n";
-	cin>>k;
-	findDel(TRP,k);
+    cout<<"\n";
+	string str;
+	cin>>str;
+	char k1 = str[0];
+	char k2 = str[1];
+	findDel(TRP,k1);
+	
+	findDel(TRP,k2);
 	inorder(TRP);
 
 	return 0;
 }
+
+
+/*
+nput1 : p 45 s 63 h 21 m 36 e 27 g 9 #
+
+output1 : inorder : LDR   :
+
+e 27 g 9 h 21 m 36 p 45 s 63
+
+input 2 ( delete  read two keys one by one without space) :  gh
+
+output2 ( after deleting g, h) : LDR :
+
+e 27 m 36 p 45 s 63
+*/
