7ea91850dd18688b278436d1ba4d31f049d676d6
Anshuman Mishra <shivanshuman021@gmail.com>
2020-12-01 16:43:25 +0530

    updated

---
+++ Graph/earliestDP.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+class lnode{
+    public:
+    int v;
+    float wt;
+    lnode(int v,float wt){
+        this->wt = wt;
+        this->v = v;
+    }
+};
+
+template<typename D>
+void printvec(vector<D>v){
+    for (int i=0;i<v.size();++i){
+        cout<<v[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+
+void addEdge(vector<list<lnode*> >&G, int u, int v,float wt)
+{
+    G[u].push_back(new lnode(v,wt));
+}
+
+
+float early(vector<list<lnode*> >&RG,vector<float>&DP,int v){
+    if(RG[v].size()==0){
+        DP[v]=0;
+        return 0;
+    }
+
+    float k = -10.0;
+    list<lnode*>::iterator it;
+    for (it = RG[v].begin();it!=RG[v].end();++it){
+        if (DP[(*it)->v]==-10.0){
+            k = fmax(k,(*it)->wt+early(RG,DP,(*it)->v));
+        }
+        else{
+            k = fmax(k,(*it)->wt+DP[(*it)->v]);
+        }
+    }
+    
+    DP[v] = k;
+    return k;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<list<lnode*> > RG(n + 1, list<lnode*>());
+    int u, v ;
+    float wt;
+
+    // n vertices
+    //m number of u and v --> m edge
+
+    //vertex weighted graph
+
+    vector<float> DP(n+1,-10.0);
+
+    float dstn_wt;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> wt;
+        if (i==n) dstn_wt = wt;
+        // i/o all child 
+        while(true){
+            cin>>u;
+            if (u==-1) break;
+            // dag -> u = i, v = u; rdag -> u = u, v = i
+            //storing wt of outgoing edge of dag
+            addEdge(RG, u, i, wt);
+        }
+        
+    }
+    //destination is 7
+    
+    cout<<early(RG,DP,n)+dstn_wt<<"\n";
+    // construct only reverse DAG
+
+    
+    return 0;
+}
+
+/*
+7
+3.1 3 5 -1
+2.0 5 -1
+4.6 4 6 -1
+1.8 7 -1
+3.0 6 -1
+4.0 7 -1
+3.7 -1
+*/
---
+++ Graph/minSpT.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <climits>
+
+using namespace std;
+
+class lnode{
+    public:
+    int v;
+    int wt;
+    lnode(int v,int wt){
+        this->v = v;
+        this->wt = wt;
+    }
+};
+
+
+void addEdge(vector<list<lnode*> > &G, int u, int v,int wt)
+{
+    G[u].push_back(new lnode(v,wt));
+    G[v].push_back(new lnode(u,wt));
+    cout<<G[u].back()->v<<" "<<G[v].back()->v<<"\n\n";
+
+}
+
+void printGraph(vector<list<lnode*> > &G)
+{
+    for (int v = 0; v < G.size(); ++v)
+    {
+        cout << v<<"\n";
+        for (list<lnode*>::iterator x = G[v].begin();x != G[v].end();++x)
+            cout<<(*x)->v<<" ";
+        cout << "\n";
+    }
+}
+
+int ExploreMst(vector<list<lnode*> >&G,vector<bool> visited,int v){
+    //if (visited[v]) return 0;
+
+    visited[v] = true;
+    int k = INT_MAX;
+
+    list<lnode*>::iterator it;
+    
+    for (it= G[v].begin();it!=G[v].end();++it)
+    {
+        if (!visited[(*it)->v])
+            k = min(k,ExploreMst(G,visited,(*it)->v));
+    }
+
+    return k;
+}
+
+int mst(vector<list<lnode*> >&G,int v,int V){
+    vector<bool> visited(V, false);
+    return ExploreMst(G, visited, v);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n;
+    vector<list<lnode*> > G(n + 1,list<lnode*> ());
+    int u, v , wt;
+    // n vertices
+    //m number of u and v --> m edge
+    while(1)
+    {
+        cin >> u ;
+        if (u==-1) break;
+        cin>> v>>wt;
+        addEdge(G, u, v, wt);
+        printGraph(G);
+    }
+
+    cout<<"here\n";
+    int k = INT_MAX;
+    for (int i=1;i<8;++i){
+        k = min(mst(G,i,8),k);
+    }
+    
+    cout<<k<<"\n";
+
+    return 0;
+}
+/*
+6
+1 2 2
+1 3 4
+1 4 1
+2 4 3
+2 5 10
+3 4 2
+3 6 5
+4 5 7
+4 6 8
+4 7 4
+5 7 6
+6 7 1
+-1
+*/
